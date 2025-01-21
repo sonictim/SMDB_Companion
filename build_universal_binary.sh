106ee0347ff301dcef978b0f5d9f2b145194757c
sonictim <soundguru@gmail.com>
2025-01-21 13:03:17 -0600

    0.3.3 - working on notarization script

--- build_universal_binary.sh
+++ build_universal_binary.sh
@@ -27,12 +27,20 @@ lipo -create -output target/universal/release/$BINARY_NAME.app/Contents/MacOS/$B
 # Verify the binary
 file target/universal/release/$BINARY_NAME.app/Contents/MacOS/$BINARY_NAME
 
+
+# codesign the binary
+codesign --sign "Developer ID Application: Tim Farrell (22D9VBGAWF)" --deep --force target/universal/release/$BINARY_NAME.app/Contents/MacOS/$BINARY_NAME
+codesign --verify --deep --strict --verbose=2 target/universal/release/$BINARY_NAME.app/Contents/MacOS/$BINARY_NAME
+
+
+
 # Define variables for paths
 APP_PATH="target/universal/release/$BINARY_NAME.app"
-ZIP_PATH="/Users/tfarrell/Library/CloudStorage/GoogleDrive-tim@farrellsound.com/Shared drives/PUBLIC/$BINARY_NAME/$BINARY_NAME.v$VERSION.zip"
-NEW_ZIP_PATH="/Users/tfarrell/Documents/Website/smdbc.com/private/$BINARY_NAME.v$VERSION.zip"
-VERSION_FILE="/Users/tfarrell/Library/CloudStorage/GoogleDrive-tim@farrellsound.com/Shared drives/PUBLIC/$BINARY_NAME/latest_ver"
-NEW_VERSION_FILE="/Users/tfarrell/Documents/Website/smdbc.com/private/latest_ver"
+ZIP_NAME="$BINARY_NAME.v$VERSION.zip"
+# ZIP_PATH="/Users/tfarrell/Library/CloudStorage/GoogleDrive-tim@farrellsound.com/Shared drives/PUBLIC/$BINARY_NAME/$BINARY_NAME.v$VERSION.zip"
+# WEB_ZIP_PATH="/Users/tfarrell/Documents/Website/smdbc.com/private/$BINARY_NAME.v$VERSION.zip"
+# GDRIVE_VERSION_FILE="/Users/tfarrell/Library/CloudStorage/GoogleDrive-tim@farrellsound.com/Shared drives/PUBLIC/$BINARY_NAME/latest_ver"
+# WEB_VERSION_FILE="/Users/tfarrell/Documents/Website/smdbc.com/private/latest_ver"
 
 # Remove old files
 /bin/rm -rf /Users/tfarrell/Library/CloudStorage/GoogleDrive-tim@farrellsound.com/Shared\ drives/PUBLIC/$BINARY_NAME/$BINARY_NAME*
@@ -41,21 +49,21 @@ NEW_VERSION_FILE="/Users/tfarrell/Documents/Website/smdbc.com/private/latest_ver
 # Create a temporary directory to hold just the .app bundle
 TEMP_DIR=$(mktemp -d)
 
-# Copy only the .app bundle to the temporary directory
-cp -R "$APP_PATH" "$TEMP_DIR/"
-
-cp -R "$APP_PATH" /Applications
+# # Copy only the .app bundle to the temporary directory
+# cp -R "$APP_PATH" "$TEMP_DIR/"
+# cp -R "$APP_PATH" /Applications
 
 # Navigate to the temporary directory
 cd "$TEMP_DIR"
 
 # Create the zip file with only the .app bundle at the root
-zip -r "$ZIP_PATH" "$(basename "$APP_PATH")"
-zip -r "$NEW_ZIP_PATH" "$(basename "$APP_PATH")"
+echo "Creating zip file for notarization..."
+zip -r "$ZIP_NAME" "$$APP_PATH"
+
 
 # Write the version number to latest_ver
-echo "$VERSION" > "$VERSION_FILE"
-echo "$VERSION" > "$NEW_VERSION_FILE"
+echo "$VERSION" > "$GDRIVE_VERSION_FILE"
+echo "$VERSION" > "$WEB_VERSION_FILE"
 
 # Clean up
 rm -rf "$TEMP_DIR"
